2efc0b96f0109f3b4a06a6822b7f515af78c2b98
Josue <josue.bills@gmail.com>
2022-01-14 01:09:23 -0800

    add review feature including validation error handling routes and basic styling.

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { reviewSchema } = require('../schemas');
 const Schema = mongoose.Schema;
 //will need to change the name of this for my places to go project
 const CampgroundSchema = new Schema({
@@ -6,7 +7,23 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await reviewSchema.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
